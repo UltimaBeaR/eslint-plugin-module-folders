0ab19d578195ddce397804fa7441b8f6161552aa
ultimabear <thebear@yandex.ru>
2025-06-21 13:57:52 +0300

    какие-то правки (не помню о чем это)

--- main/src/correctImportsRule.js
+++ main/src/correctImportsRule.js
@@ -1,15 +1,15 @@
-import path from 'node:path';
-import { projectNodeModulesAbsDir } from './internal/paths.js';
-import { resolveTsImport } from './internal/resolveTsImport.js';
-import { getTsFileModuleInfo } from './internal/getTsFileModuleInfo.js';
+import path from "node:path";
+import { projectNodeModulesAbsDir } from "./internal/paths.js";
+import { resolveTsImport } from "./internal/resolveTsImport.js";
+import { getTsFileModuleInfo } from "./internal/getTsFileModuleInfo.js";
 /**
  * @type {import('eslint').Rule.RuleModule}
  */
 export const correctImportsRule = {
   meta: {
-    type: 'problem',
+    type: "problem",
     docs: {
-      description: 'Только корректный импорт папок-модулей',
+      description: "Только корректный импорт папок-модулей",
     },
     schema: [],
   },
@@ -27,11 +27,14 @@ function create(context) {
 
       // Если путь к файлу импорта резолвится не как строка то пропускаем.
       // Не знаю что это за кейсы такие, но вроде как в обычном случае всегда тут строка
-      if (typeof importingExpression !== 'string') {
+      if (typeof importingExpression !== "string") {
         return;
       }
 
-      const importingFileName = resolveTsImport(importingExpression, imporerFileName);
+      const importingFileName = resolveTsImport(
+        importingExpression,
+        imporerFileName
+      );
 
       // undefined будет если это не js/ts файл а файл ресурсов/стилей
       // такие файлы пропускаем
@@ -47,17 +50,52 @@ function create(context) {
       const importingModuleInfo = getTsFileModuleInfo(importingFileName);
       const importerModuleInfo = getTsFileModuleInfo(imporerFileName);
 
-      console.log('###### ImportDeclaration', {
-        importingFileName,
-        importingModuleInfo,
+      /*
+        TODO:
+        Общая логика правил, не только для этого правила конкретного.
+        Можно попробовать ее вынести в хэлпер и переиспользовать в разных правилах.
+        Ну либо пусть будет одно правило но с разными сообщениями.
+
+        Отдельное правило, что в определенной папке должны быть ТОЛЬКО модули и больше никаких других файлов
+          if текущий файл находится в этой папке (из списка, должен задаваться конфигом), при этом
+          этот файл не файл модуля и в его пути нигде нету модулей и на уровне его основной папки нет файлов-модулей
+            ошибка - найден файл вне модулей, хотя папка должна быть только с модулями внутри
+          else
+            все ок.
+
+        Отдельное правило, проверяющее сам файл текущий на коммент @module топ левел.
+          Проверяем по кэшу модульный ли этот файл, если нет, игнорим этот файл (значит коммент не топ левел)
+          if в кэше есть - проверяем что выше по пути импорта нет других модулей.
+            if есть
+              ругаемся что не может быть вложенных модулей.
+
+        if импортируемый файл это модуль
+          if этот модуль является тем же модулем откуда идет импорт (ситуация если импорт вида ../index.ts)
+            то пишем что файлы модулей могут быть использованы только для внешнего кода
+          else
+            все ок - это импорт чужого модуля (тут дальше можно правила fsd слайсов и т.д. подключать)
+        else
+          if в пути импорта где то есть модуль (ищем только первый, не важно какой)
+            if этот модуль является тем же модулем откуда идет импорт (в случае если текущий файл тоже в модуле)
+              все ок - импорт из кишков своего же модуля
+            else
+              ругаемся что доступ напрямую к файла из кишков модуля идет напрямую а не через импорт файла модуля.
+          else
+            все ок - импорт из какого-то кода, который вообще не усавствует в модульной системе
+
+      */
+
+      console.log("###### ImportDeclaration", {
         imporerFileName,
         importerModuleInfo,
+        importingFileName,
+        importingModuleInfo,
       });
 
       if (false) {
         context.report({
           node,
-          message: 'Не нашел коммент // @module-folder',
+          message: "Не нашел коммент // @module-folder",
         });
       }
     },
--- main/src/internal/getTsFileModuleInfo.js
+++ main/src/internal/getTsFileModuleInfo.js
@@ -1,5 +1,5 @@
-import { getImportingFileSubfolderDirs } from './getImportingFileSubfolderDirs.js';
-import { moduleTreeCache } from './moduleTree.js';
+import { getImportingFileSubfolderDirs } from "./getImportingFileSubfolderDirs.js";
+import { moduleTreeCache } from "./moduleTree.js";
 
 /**
  * @param {string} tsFilePath
@@ -11,7 +11,9 @@ export function getTsFileModuleInfo(tsFilePath) {
     if (moduleTreeCache.fileFolders.has(subFolderDir)) {
       const filesMap = moduleTreeCache.fileFolders.get(subFolderDir);
 
-      const isModule = [...filesMap.values()].some((fileCache) => fileCache.isModule);
+      const isModule = [...filesMap.values()].some(
+        (fileCache) => fileCache.isModule
+      );
 
       return isModule;
     }
@@ -20,9 +22,13 @@ export function getTsFileModuleInfo(tsFilePath) {
   });
 
   const tsFilePathIsModule =
-    moduleTreeCache.files.has(tsFilePath) && moduleTreeCache.files.get(tsFilePath).isModule;
+    moduleTreeCache.files.has(tsFilePath) &&
+    moduleTreeCache.files.get(tsFilePath).isModule;
 
   return {
+    // moduleFolder - путь до папки с модулем,
+    //
+
     pathIsModule: tsFilePathIsModule,
     moduleFolders: moduleSubfolders,
   };
